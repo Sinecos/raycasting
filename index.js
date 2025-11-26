2555fa5b5faa2178f063080b5b6a8fb72f78fe8a
Theal Altake <zufa.altake@gmail.com>
2025-11-26 15:56:03 +0200

    init_

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 "use strict";
-console.log("Hello world");
+(() => {
+    const game = document.getElementById("game");
+    if (game === null) {
+        throw new Error("No canvas with id 'game' is found");
+    }
+    const ctx = game === null || game === void 0 ? void 0 : game.getContext("2d");
+    if (ctx === null) {
+        throw new Error("2D context is not supported");
+    }
+    console.log(ctx);
+})();
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello world");
+(() => {
+    const game = document.getElementById("game") as (HTMLCanvasElement | null);
+
+    if(game === null){
+        throw new Error("No canvas with id 'game' is found");
+
+    }
+    const ctx = game?.getContext("2d");
+    if(ctx === null){
+        throw new Error("2D context is not supported");
+    }
+    console.log(ctx);
+})()
