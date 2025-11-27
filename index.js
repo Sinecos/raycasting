cb30f0389cbc9f8bc547fc91366b3ff631ebdd17
Theal Altake <zufa.altake@gmail.com>
2025-11-27 10:36:19 +0200

    Two points and line

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
 "use strict";
 const GRID_ROWS = 10;
 const GRID_COLS = 10;
+class Vector2 {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    array() {
+        return [this.x, this.y];
+    }
+}
+function fillCircle(ctx, center, radius) {
+    ctx.beginPath();
+    ctx.arc(...center.array(), 0.2, 0, 2 * Math.PI);
+    ctx.fill();
+}
+function strokeLine(ctx, p1, p2) {
+    ctx.beginPath();
+    ctx.moveTo(...p1.array());
+    ctx.lineTo(...p2.array());
+    ctx.stroke();
+}
 (() => {
     const game = document.getElementById("game");
     if (game === null) {
@@ -14,18 +34,20 @@ const GRID_COLS = 10;
     }
     ctx.fillStyle = "#181818";
     ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-    const cell_width = ctx.canvas.width / GRID_COLS;
-    const cell_height = ctx.canvas.height / GRID_ROWS;
+    ctx.scale(ctx.canvas.width / GRID_COLS, ctx.canvas.height / GRID_ROWS);
+    ctx.lineWidth = 0.02;
     ctx.strokeStyle = "#303030";
     for (let x = 0; x <= GRID_COLS; ++x) {
-        ctx.moveTo(x * cell_width, 0);
-        ctx.lineTo(x * cell_width, ctx.canvas.height);
-        ctx.stroke();
+        strokeLine(ctx, new Vector2(x, 0), new Vector2(x, GRID_ROWS));
     }
     for (let y = 0; y <= GRID_ROWS; ++y) {
-        ctx.moveTo(0, cell_height * y);
-        ctx.lineTo(ctx.canvas.width, y * cell_height);
-        ctx.stroke();
+        strokeLine(ctx, new Vector2(0, y), new Vector2(GRID_COLS, y));
     }
-    console.log(ctx);
+    const p1 = new Vector2(GRID_COLS * 0.43, GRID_ROWS * 0.33);
+    const p2 = new Vector2(GRID_COLS * 0.33, GRID_ROWS * 0.43);
+    ctx.fillStyle = "magenta";
+    fillCircle(ctx, p1, 0.2);
+    fillCircle(ctx, p2, 0.2);
+    ctx.strokeStyle = "magenta";
+    strokeLine(ctx, p1, p2);
 })();
